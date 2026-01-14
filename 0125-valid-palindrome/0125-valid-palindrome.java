2488b346e92541b008068c53b58c8a2497b31398
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-01-14 23:21:57 +0530

    Time: 3 ms (63.51%), Space: 44.7 MB (52.46%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 1){
+            return true;
+        }
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch >= 'a' && ch<='z'){
+                sb.append(ch);
+            }else if(ch >= 'A' && ch<= 'Z'){
+                ch = (char)(ch -'A'+ 'a');
+                sb.append(ch);
+            }else if(ch >= '0' && ch <= '9'){
+                 sb.append(ch);
+            }
+        }
+
+        int start = 0;
+        int end = sb.length()-1;
+        while(start <= end){
+            int echar = sb.charAt(end);
+            int schar = sb.charAt(start);
+            if(echar != schar){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+}
