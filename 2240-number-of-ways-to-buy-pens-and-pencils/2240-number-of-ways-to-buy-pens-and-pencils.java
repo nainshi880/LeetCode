dcd33f7d96826d74daf50158c25df2c0e56e8591
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-02-13 19:23:26 +0530

    Time: 9 ms (96.05%), Space: 42.5 MB (18.08%) - LeetHub

--- 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -1,13 +1,29 @@
 class Solution {
     public long waysToBuyPensPencils(int total, int cost1, int cost2) {
         
-        long ways = 0;
-        long penCost = 0;
-        while(penCost <= total){
-            long remain = total - penCost;
-            long pencil = remain/cost2 +1;
-            ways += pencil;
-            penCost += cost1;
+        // long ways = 0;
+        // long penCost = 0;
+        // while(penCost <= total){
+        //     long remain = total - penCost;
+        //     long pencil = remain/cost2 +1;
+        //     ways += pencil;
+        //     penCost += cost1;
+        // }
+        // return ways;
+
+            long ways = 0;
+        long remain = total;
+        
+        if(cost1 <= cost2){
+            while(remain >= 0){
+                ways += (remain/cost1)+1;
+                remain -= cost2;
+            }
+        }else{
+            while(remain >= 0){
+                ways += (remain/cost2)+1;
+                remain -= cost1;
+            }
         }
         return ways;
         
