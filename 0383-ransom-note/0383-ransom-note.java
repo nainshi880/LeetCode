3d3ac4f805f2b37b743969c8173943eb2224a810
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-02-25 20:16:51 +0530

    Time: 1 ms (99.84%), Space: 46.4 MB (81.63%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,19 +2,25 @@ class Solution {
     public boolean canConstruct(String r, String m) {
         
         int[] freq = new int[26];
-        
+        int count =0;
         for(int i=0;i<m.length();i++){
             freq[m.charAt(i)-'a']++;
         }
         for(int i=0;i<r.length();i++){
           int idx =  r.charAt(i)-'a' ;
-          if(freq[idx] == 0){
-            return false;
+          if(freq[idx] > 0){
+             freq[idx]--;
+             count++;
+          }else{
+            count = 0;
+            break;
           }
 
-           freq[idx]--;
+           if(count == r.length()){
+            return true;
+           }
         }
        
-        return true;
+        return false;
     }
 }
