8b825ae0ffd7e851291e0c6e22406c8c74e337ff
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-04-12 15:34:19 +0530

    Time: 6 ms (8.55%), Space: 47.2 MB (40.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,25 @@
+
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
-        int[][] dp = new int[n+1][m+1];
-
-        for(int i=0;i<=n;i++){
-
-            dp[i][0] = i;
-        }
-       for(int j =0;j<=m;j++){
-        dp[0][j] = j;
-
-       }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1];
-                }else{
-                     dp[i][j] = Math.min(
-                        dp[i-1][j-1] + 1, Math.min(
-                            dp[i][j-1] + 1,            
-                            dp[i-1][j] + 1));
-                }
+    public int minDistance(String w1, String w2) {
+        int m = w1.length();
+        int n = w2.length();
+        if(w1.equals(w2)) return 0;
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j] = -1;
             }
         }
-        return dp[n][m];
+        return edit(m-1,n-1,new StringBuilder(w1),new StringBuilder(w2),dp);
+    }
+    public static int edit(int i, int j,StringBuilder s1, StringBuilder s2,int [][] dp){
+        if(i < 0) return j+1;
+        if(j < 0) return i+1;
+        if(s1.charAt(i) == s2.charAt(j)) return edit(i-1,j-1,s1,s2,dp);
+        if(dp[i][j] != -1) return dp[i][j];
+        int replace = 1 + edit(i-1,j-1,s1,s2,dp);
+        int remove = 1 + edit(i-1,j,s1,s2,dp);
+        int insert = 1 + edit(i,j-1,s1,s2,dp);
+         return dp[i][j] = Math.min(replace,Math.min(remove,insert));
     }
 }
