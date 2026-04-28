790e3f44207d4be558e3300ed5b17aeeb2008fd6
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-04-28 21:58:30 +0530

    Time: 0 ms (100%), Space: 43.3 MB (72.68%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,12 +1,22 @@
 class Solution {
     public int hIndex(int[] c) {
-        Arrays.sort(c);
        int n = c.length;
-       for(int i=0;i<n;i++){
-        if(c[i] >= n-i){
-            return n-i;
+       int[] b = new int[n+1];
+       for(int i =0;i<n;i++){
+        int x = c[i];
+        if(x >= n){
+            b[n]++;
+        }else{
+            b[x]++;
         }
        }
+       int count = 0;
+       for(int i=n;i>0;i--){
+        count += b[i];
+        if(count >= i){
+            return i;
+        }
+       } 
        return 0;
     }
 }
