402c96ae0140eeb9f32686560c75e3e8c196b94e
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-04-28 21:51:10 +0530

    Time: 1 ms (24.75%), Space: 50.4 MB (12.84%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] c) {
+        int n = c.length;
+        for(int i=0;i<n;i++){
+            if(c[i] >= n-i){
+                return n-i;
+            }
+        }
+        return 0;
+    }
+}
