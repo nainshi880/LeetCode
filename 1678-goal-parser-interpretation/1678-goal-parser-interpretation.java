184a51c694adfbe06a6ac8c75edb8317c5cb81dc
Nainshi Jaiswal <nainshijaiswal24@gmail.com>
2026-05-30 00:11:55 +0530

    Time: 0 ms (100%), Space: 43.1 MB (18.07%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String interpret(String c) {
+        StringBuilder s = new StringBuilder(c.length());
+
+        for(int i=0;i<c.length();i++){
+            if(c.charAt(i) == 'G') s.append('G');
+            if(c.charAt(i) == '('){
+                if(c.charAt(i+1) == ')'){
+                    s.append('o');
+                    i++;
+                }else{
+                    s.append("al");
+                    i = i+3;
+                }
+            }
+        }
+        return s.toString();
+    }
+}
